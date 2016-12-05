7c0868aa41a4dabd266ca08fa325ab4f3e2d8346
Owen Flannigan <owenflannigan@Students-MacBook-Pro.local>
2016-12-04 21:53:57 -0800

    Add completed code

--- index.js
+++ index.js
@@ -3,10 +3,32 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-    res.send('Hello world');
+var users = {
+    owen: {
+        password: 'password'
+    },
+    jessica: {
+        password: 'password'
+    }
+};
+
+app.get('/login', function (req, res) {
+    var username = req.query.username;
+    var password = req.query.password;
+
+    var user = users[username];
+    if (user) {
+        if (user.password == password) {
+            res.send('Logged in as ' + username);
+        } else {
+            res.send('Incorrect password for ' + username);
+        }
+    } else {
+        res.send('User does not exist');
+    }
+
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('Node app listening on port 3000!');
 });
