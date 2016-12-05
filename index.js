01d71c1f57365a840aced22eec6f295174780c1d
Owen Flannigan <owenflannigan@Students-MacBook-Pro.local>
2016-12-04 20:43:08 -0800

    Update starter code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
+app.use(express.static('public'))
+
+app.get('/login', function(req, res) {
+    res.send('Hello world');
 });
 
 app.listen(3000, function() {
